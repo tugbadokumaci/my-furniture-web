0b72fbe10036d519fd10421bf0283eee15d66c6c
tugbadokumaci <cennetinagaci@gmail.com>
2023-12-21 12:26:12 +0300

    uı completed

--- furniture ecommerce/Controllers/DefaultController.cs
+++ furniture ecommerce/Controllers/DefaultController.cs
@@ -9,6 +9,7 @@ namespace UI.Controllers
 {
 	public class DefaultController : Controller
 	{
+
         private readonly IHttpClientFactory _httpClientFactory;
 
         public DefaultController(IHttpClientFactory httpClientFactory)
@@ -20,20 +21,60 @@ namespace UI.Controllers
 		{
 			return View();
 		}
-        public ActionResult LivingRooms()
+        public async Task<IActionResult> LivingRooms()
         {
+
+            ViewBag.SofaModel = await FetchCategoryData(3);
+            ViewBag.ChairModel = await FetchCategoryData(1);
+
+
             return View();
         }
-        public ActionResult DiningRooms()
+
+        // Method to fetch category data from the API
+        private async Task<List<Product>> FetchCategoryData(int categoryId)
         {
+            List<Product> categoryData = null;
+
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7119/"); // Update with your API base address
+
+                var response = await client.GetAsync($"/categories/{categoryId}");
+                if (response.IsSuccessStatusCode)
+                {
+                    categoryData = await response.Content.ReadFromJsonAsync<List<Product>>(); // Update with your Product model
+                }
+                else
+                {
+                    // Handle error
+                }
+            }
+
+            return categoryData;
+        }
+        public async Task<IActionResult> DiningRooms()
+        {
+            ViewBag.DiningTableModel = await FetchCategoryData(7);
+            ViewBag.SideboardsModel = await FetchCategoryData(8);
+            ViewBag.KitchenTableModel = await FetchCategoryData(9);
+
+
             return View();
         }
-        public ActionResult Bedrooms()
+        public async Task<IActionResult> Bedrooms()
         {
+            ViewBag.BedModel = await FetchCategoryData(4);
+            ViewBag.MirrorModel = await FetchCategoryData(6);
+
+
             return View();
         }
-        public ActionResult HomeDecor()
+        public async Task<IActionResult> HomeDecor()
         {
+            ViewBag.PillowModel = await FetchCategoryData(10);
+
+
             return View();
         }
         public async Task<IActionResult> DetailAsync(int id)
